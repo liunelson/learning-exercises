eb60f300eeb6a0d4490a694637b205d5552ca0f7
Lai Chung Liu <liu.nelson@gmail.com>
2019-07-08 13:41:58 -0400

    Added gitignore for jupyter checkpoints

---
+++ nielsen2015/nielsen2015.py
@@ -0,0 +1,16 @@
+#%% Print "Hello World!"
+msg = "Hello World!"
+print(msg)
+
+#%% Testing plotting.
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
+
+
+
+#%%
